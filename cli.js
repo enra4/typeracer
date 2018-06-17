c213d5baa9a316bec638b2984a7d6c43797e5075
enra4 <cudcsb@gmail.com>
2018-06-17 14:41:13 +0200

    .. nice

--- cli.js
+++ cli.js
@@ -30,22 +30,24 @@ const cli = meow(`
 	}
 })
 
-if (cli.flags.host && cli.flags.name === undefined) {
-	console.log('please specify your name with -n <name>')
-	process.exit()
-}
+if (!cli.flags.host && !cli.flags.name) {
+	singleplayer()
+} else {
+	if (cli.flags.host && cli.flags.name === undefined) {
+		console.log('please specify your name with -n <name>')
+		process.exit()
+	}
 
-if (cli.flags.name && cli.flags.host === undefined) {
-	console.log('please specify your host with -h <name/address>')
-	process.exit()
-}
+	if (cli.flags.name && cli.flags.host === undefined) {
+		console.log('please specify your host with -h <name/address>')
+		process.exit()
+	}
 
-if (cli.flags.name.length > 16) {
-	console.log('name needs to be 16 char or less :)')
-	process.exit()
-}
+	if (cli.flags.name.length > 16) {
+		console.log('name needs to be 16 char or less :)')
+		process.exit()
+	}
 
-if (cli.flags.host && cli.flags.name) {
 	let port = 1234
 
 	let index = cli.flags.host.indexOf(':')
@@ -54,7 +56,5 @@ if (cli.flags.host && cli.flags.name) {
 		cli.flags.host = cli.flags.host.slice(0, index)
 	}
 
-	multiplayer(cli.flags.host, port, cli.flags.name)
-} else {
-	singleplayer()
+	multiplayer(cli.flags.host, port, cli.flags.name)	
 }
